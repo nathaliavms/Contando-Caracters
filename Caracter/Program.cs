d584a326fc85a2fce283fbaed2717bad1ab4bb30
Nathalia Vieira <101611468+nathaliavms@users.noreply.github.com>
2022-09-05 11:47:27 -0300

    Add files via upload

---
+++ Caracter/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Caracter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] frase;
+            string maiorPalvra = "";
+
+            for (; ; )
+            {
+                frase = Console.ReadLine().Split(' ');
+                if (frase[0] == "0")
+                {
+                    Console.WriteLine();
+                    break;
+                }
+                string palavraSeparada = "";
+
+                for (int i = 0; i < frase.Length; i++)
+                {
+                    if (frase[i].Length >= maiorPalvra.Length)
+                    {
+                        maiorPalvra = frase[i];
+                    }
+                    if (i == (frase.Length - 1))
+                    {
+                        palavraSeparada += $"{frase[i].Length}";
+                    }
+                    else
+                    {
+                        palavraSeparada += $"{frase[i].Length}-";
+                    }
+                }
+
+                Console.WriteLine(palavraSeparada);
+                Console.WriteLine($"The biggest word: {maiorPalvra}");
+            }
+
+            
+        }
+    }
+}
+
